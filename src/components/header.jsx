e4d0c37287f69111dd0757f4531793b08032dc7e
Johnston P S <2007.johnston@gmail.com>
2026-01-18 12:40:01 +0530

    initial commit

---
+++ src/components/header.jsx
@@ -0,0 +1,10 @@
+import thattukada from '../assets/thattukada.png'
+
+export default function Header() {
+  return (
+    <header>
+      <img src={thattukada} alt="thattukada" />
+      <h1>Thattukada Recipes</h1>
+    </header>
+  )
+}
---
+++ src/components/main.jsx
@@ -0,0 +1,10 @@
+export default function Main() {
+  return (
+    <main>
+      <form>
+        <input type="text" placeholder="eg: egg"/>
+        <button className="addButton">Add Ingredient</button>
+      </form>
+    </main>
+  )
+}
